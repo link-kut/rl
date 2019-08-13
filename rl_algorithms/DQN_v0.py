d565769c346c2529bcbd66e3c225436298d8f318
Youn-Hee Han <yh21.han@gmail.com>
2019-08-14 08:37:20 +0900

    ...

--- rl_algorithms/DQN_v0.py
+++ rl_algorithms/DQN_v0.py
@@ -1,18 +1,10 @@
-import gym
 import math
 import random
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 from collections import namedtuple, deque
-from itertools import count
-from PIL import Image
 
 import torch
-import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import torchvision.transforms as T
 
 from conf.constants_mine import DEEP_LEARNING_MODEL
 from conf.names import ModelName
